c17564204bf8f43a813371e0549af55916efc15a
rYmS26 <120410056+rYmS26@users.noreply.github.com>
2023-01-17 20:17:44 +0800

    Add files via upload

---
+++ header.h
@@ -0,0 +1,11 @@
+tampilan (char judd [49]="")
+{
+ if (strcmp(judd,"header")==0)
+{
+ cout<<"Nama     = Royhan Muhammad Sabila"<<endl;
+ cout<<"Kelas    = X PPLG"<<endl;
+ cout<<"Username = operationstrix"<<endl;
+ cout<<"Password = siryessir"<<endl;
+ cout<<" "<<endl;
+}
+}
